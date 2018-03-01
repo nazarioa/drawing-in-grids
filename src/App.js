3c8451e4e7593af37a7d8c436bf1c17f4733887c
Nazario A. Ayala <nazario@niztech.com>
2018-02-28 17:48:35 -0800

    Addedon hover draw functionality.

--- src/App.js
+++ src/App.js
@@ -6,9 +6,34 @@ import './App.css';
 class App extends Component {
 
   render() {
+    const cellWidthHeight = 20;
+
+    let availableSpace = {
+      width: window.innerWidth || document.body.clientWidth,
+      height: (window.innerHeight || document.body.clientHeight) - 100
+      // 100  = 60 (.AppHeader height)
+      //      + 10*2 (.App-header padding)
+      //      + 10*2 (.App-grid padding);
+    }
+
+    let gridWidth = Math.round(availableSpace.width / cellWidthHeight);
+    let gridHeight = Math.round(availableSpace.height / cellWidthHeight);
+
+    let gridTemplateStyle = {
+      gridTemplateRows: 'repeat(' + gridHeight + ', ' + cellWidthHeight + 'px)',
+      gridTemplateColumns: 'repeat(' + gridWidth + ', ' + cellWidthHeight + 'px)',
+      width: (gridWidth * cellWidthHeight) + 'px',
+      height: (gridHeight * cellWidthHeight) + 'px'
+    };
+
+    let cellStyle = {
+      width: cellWidthHeight + 'px',
+      height: cellWidthHeight + 'px'
+    };
+
     let cells = [];
-    for (let i = 0; i < 64; i++) {
-      cells.push(<Cell position={i} className="cell-{i}"/>);
+    for (let i = 0; i < (gridWidth * gridHeight); i++) {
+      cells.push(<Cell key={i} className="cell-{i}" style={cellStyle} />);
     }
 
     return (
@@ -16,7 +41,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Draw!</h1>
         </header>
-        <div className="App-grid">
+        <div className="App-grid" style={gridTemplateStyle}>
           {cells}
         </div>
       </div>
--- src/Cell/Cell.js
+++ src/Cell/Cell.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import classnames from 'classnames';
 
 import './Cell.css';
 
 class Cell extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {active: false};
+
+    this.handleOnMouseOut = this.handleOnMouseOut.bind(this);
+    this.handleOnHover = this.handleOnHover.bind(this);
+  }
+
+
+  handleOnHover(e) {
+    this.setState({active: true});
+  }
+
+  handleOnMouseOut(e) {
+    let p = setTimeout(() => this.setState({active: false}), 6000);
+  }
+
   render() {
+    let classes = classnames('Cell-pulp', {active: !!this.state.active})
+
     return (
-      <div className="Cell">
-        <div className="Cell-pulp"></div>
+      <div className="Cell" onMouseLeave={this.handleOnMouseOut} onMouseEnter={this.handleOnHover}>
+        <div className={classes}></div>
       </div>
     );
   }
